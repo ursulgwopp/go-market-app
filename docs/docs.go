d6e10c46f8c8ff30950bf0410a8414c53f7d2ee7
ursulgwopp <ursuldm@gmail.com>
2024-12-14 15:07:15 +0300

    deploying...

--- docs/docs.go
+++ docs/docs.go
@@ -1076,7 +1076,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:1334",
+	Host:             "go-market-app-production.up.railway.app",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Market App API",
